6e43d5f5a0decf1b3c1c5176f3d6b1355dce67c8
Hugo-Brener <hbrener82@gmail.com>
2023-02-20 17:29:10 -0300

    landing page para o site marcos ribeiro

--- site-desktop-v2/javascript/main.js
+++ site-desktop-v2/javascript/main.js
@@ -1,2 +1,17 @@
+foto = document.getElementsByClassName("bg-intro")
+fotoBG = Array.from(foto)
+
+function slide() {
+    
+    
+    console.log(fotoBG)
+    fotoBG.forEach(element => {
+      setTimeout(element.setAttribute('id','selected'), 5000)
+         
+    });
+    
+}
 
 
+
+window.addEventListener("load", slide)
